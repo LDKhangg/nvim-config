99dca2b7dc072dfa22b1e099c0ec5dd419135185
LDKhangg <lekhangmc12@gmail.com>
2026-08-15 12:58:53 +0700

    feat: migrate to cyberdream colorscheme and polish neotree

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,44 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "scottmckendry/cyberdream.nvim",
     priority = 1000, -- Đảm bảo nó load trước các plugin khác
     lazy = false,    -- Load ngay khi mở Neovim
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = {
-          light = "latte",
-          dark = "mocha",
-        },
-        transparent_background = true, 
-        integrations = {
-          telescope = true,
-          neotree = true,
-          treesitter = true,
-          mason = true,
-          indent_blankline = {
-            enabled = true,
-          },
-        },
+      require("cyberdream").setup({
+        variant = "default",
+        transparent = true,
+        saturation = 1,
+        italic_comments = false,
+        borderless_pickers = false,
+        terminal_colors = true,
+        overrides = function(colors)
+          return {
+            NeoTreeNormal = { bg = "NONE", fg = colors.fg },
+            NeoTreeNormalNC = { bg = "NONE", fg = colors.fg },
+            NeoTreeEndOfBuffer = { bg = "NONE", fg = colors.bg },
+            NeoTreeWinSeparator = { bg = "NONE", fg = colors.bg_highlight },
+            NeoTreeCursorLine = { bg = colors.bg_alt },
+            NeoTreeDirectoryName = { fg = colors.pink },
+            NeoTreeDirectoryIcon = { fg = colors.pink },
+            NeoTreeRootName = { fg = colors.fg, bold = true },
+            NeoTreeFileName = { fg = colors.fg },
+            NeoTreeFileNameOpened = { fg = colors.fg },
+            NeoTreeFileIcon = { fg = colors.grey },
+            NeoTreeDimText = { fg = colors.grey },
+            NeoTreeHiddenByName = { fg = colors.grey },
+            NeoTreeIndentMarker = { fg = colors.bg_highlight },
+            NeoTreeExpander = { fg = colors.pink },
+            NeoTreeGitAdded = { fg = colors.green },
+            NeoTreeGitModified = { fg = colors.orange },
+            NeoTreeGitDeleted = { fg = colors.red },
+            NeoTreeFloatBorder = { fg = colors.bg_highlight, bg = "NONE" },
+            NeoTreeFloatTitle = { fg = colors.bg, bg = colors.blue, bold = true },
+            NeoTreeTitleBar = { fg = colors.fg, bg = "NONE", bold = true },
+          }
+        end,
       })
 
-
-      vim.cmd.colorscheme "catppuccin-mocha"
+      vim.cmd.colorscheme("cyberdream")
     end,
   },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,31 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      window = {
+        width = 34,
+      },
+      default_component_configs = {
+        indent = {
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          expander_collapsed = "",
+          expander_expanded = "",
+        },
+        icon = {
+          folder_closed = "󰉋",
+          folder_open = "󰝰",
+          folder_empty = "󰉖",
+          folder_empty_open = "󰷏",
+          default = "󰈔",
+          highlight = "NeoTreeFileIcon",
+        },
+        name = {
+          highlight = "NeoTreeFileName",
+          use_git_status_colors = false,
+          use_filtered_colors = false,
+        },
+      },
       filesystem = {
         follow_current_file = {
           enabled = true, -- Tự động focus và expand đến file đang mở
